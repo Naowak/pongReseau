1d55f5b3898607290023c94648741fc1b3989b0a
RadeMathis <rade.mathis@free.fr>
2015-11-20 09:45:50 +0100

    creation des Socket (com/Sock.java)

---
+++ pong/com/Sock.java
@@ -0,0 +1,47 @@
+package pong.com;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Sock{
+
+	public Sock(String ipv4){
+		try{
+			socket = new Socket(ipv4, PORT);
+		} catch (IOException e) {
+			System.out.println(e);
+			System.out.println("Creation d'un ServerSocket");
+			try{
+				servSocket = new ServerSocket(PORT);
+				socket = servSocket.accept();
+				System.out.println("Client accepte, debut de connection." + socket.toString());
+			} catch (IOException e2) {
+				System.out.println(e2);
+			}
+
+		}
+	}
+
+	public void close(){
+		try{
+			if(socket != null)
+				socket.close();
+			if(servSocket != null)
+				servSocket.close();
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+
+	public static void main(String argv[]){
+		Sock my_Sock = new Sock(argv[0]);
+
+		my_Sock.close();
+	}
+
+
+	private final static int PORT = 10654;
+	private static Socket socket;
+	private static ServerSocket servSocket;
+}
