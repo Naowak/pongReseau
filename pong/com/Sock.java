d68d504cefd1b9e01bebf7dc85e762762df42bbf
RadeMathis <rade.mathis@free.fr>
2015-11-20 10:18:31 +0100

    creation de la mehode de commun,cation

--- pong/com/Sock.java
+++ pong/com/Sock.java
@@ -16,12 +16,20 @@ public class Sock{
 			try{
 				servSocket = new ServerSocket(PORT);
 				socket = servSocket.accept();
-				System.out.println("Client accepte, debut de connection." + socket.toString());
+				System.out.println("Client accepte, debut de connection.");
+				System.out.println(socket.toString());
 			} catch (IOException e2) {
 				System.out.println(e2);
 			}
 
 		}
+		inStream  = socket.getInputStream();
+		outStream = socket.getOutputStream();
+	}
+
+	public int communicate(int envoyer){
+		outStream.write(envoyer);
+		return inStream.read();
 	}
 
 	public void close(){
@@ -38,6 +46,11 @@ public class Sock{
 	public static void main(String argv[]){
 		Sock my_Sock = new Sock(argv[0]);
 
+		System.out.println(communicate(1));
+		System.out.println(communicate(2));
+		System.out.println(communicate(3));
+		System.out.println(communicate(4));
+
 		my_Sock.close();
 	}
 
@@ -45,6 +58,9 @@ public class Sock{
 	private final static int PORT = 10654;
 	private static Socket socket;
 	private static ServerSocket servSocket;
+
+	private InputStream inStream;
+	private OutputStream outStream;
 }
 
 // 10.3.7.126
