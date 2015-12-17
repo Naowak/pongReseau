d61de0195ab0ebacde71a6a019a6e539bb3385d3
RadeMathis <rade.mathis@free.fr>
2015-12-17 15:05:03 +0100

    suppression du delai de deux econdes au lance

--- pong/src/Pong.java
+++ pong/src/Pong.java
@@ -106,7 +106,8 @@ public class Pong extends JPanel implements KeyListener {
 
         pointsJoueurGauche = 0;
         pointsJoueurGauche = 0;
-        restartTime = (Instant.now()).plusSeconds(2);
+        restartTime = Instant.now();
+        //restartTime = (Instant.now()).plusSeconds(2);
 
         racket1 = new Racket(20, (SIZE_PONG_Y / 2) - 50, 1);
         racket2 = new Racket(SIZE_PONG_X - 20 - 1, (SIZE_PONG_Y / 2) - 50, 2);
